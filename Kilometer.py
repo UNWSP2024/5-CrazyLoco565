884ea07b3094d9a2a356fef09ae9c931721b902e
CrazyLoco565 <giancarlo@myluciano.com>
2025-10-01 13:37:31 -0500

    Add files via upload
    These are the required programs for the week.

---
+++ Kilometer.py
@@ -0,0 +1,10 @@
+#Miles = kilometers x 0.6214
+
+# Input should be in kilometers, output should be in miles.
+
+kilometers = (int(input('Enter an amount of Kilometers:\n')))
+
+Miles = (kilometers * 0.6214)
+
+print (f'The amount in miles is:\n{Miles:.4} miles.')
+
---
+++ Math Quiz.py
@@ -0,0 +1,24 @@
+input ('Here is your first problem:')
+print('143+153')
+program_1 = input()
+if program_1 == "296":
+    print('Good job, that\'s right!')
+if program_1 != "296":
+    print(f'Sorry, that\'s not correct.\nThe correct answer is 296')
+
+
+input ('Here is your second problem:')
+print('231+252')
+program_2 = input()
+if program_2 == "483":
+    print('Good job, that\'s right!')
+if program_2 != "483":
+    print(f'Sorry, that\'s not correct.\nThe correct answer is 483')
+
+input ('Here is your third problem:')
+print('314+241')
+program_3 = input()
+if program_3 == "555":
+    print('Good job, that\'s right!')
+if program_3 != "555":
+    print(f'Sorry, that\'s not correct.\nThe correct answer is 555')
